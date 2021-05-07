e4fee5d65951606b5254985d66b34624df011137
f1ac <osoianu.adrian@gmail.com>
2021-05-07 04:17:01 +0300

    Update Unpack to OGG.bat
    Added support for NieR Automata game archives, streamlined user interaction text

--- Unpack to OGG.bat
+++ Unpack to OGG.bat
@@ -1,4 +1,5 @@
-FOR %%a IN ("Game Files\*.PCK") DO ("Tools\quickbms.exe" "Tools\wavescan.bms" "Game Files" "Tools\Decoding")
+FOR %%a IN ("Game Files\*.pck") DO ("Tools\quickbms.exe" -k "Tools\wavescan.bms" "Game Files" "Tools\Decoding")
+FOR %%a IN ("Game Files\*.wsp") DO ("Tools\quickbms.exe" -k "Tools\wavescan.bms" "Game Files" "Tools\Decoding")
 FOR %%b IN ("Game Files\*.BNK") DO ("Tools\bnkextr.exe" "%%b" & MOVE *.wav "Tools\Decoding")
 FOR %%c IN (Tools\Decoding\*.WAV) DO ("Tools\ww2ogg.exe" "%%c" --pcb Tools\packed_codebooks_aoTuV_603.bin & DEL "%%c")
 FOR %%d IN (Tools\Decoding\*.OGG) DO ("Tools\revorb.exe" "%%d" & MOVE "%%d" "OGG")
@@ -32,30 +33,31 @@ echo      Watch out, it's the tornado!
 echo.
 echo -------------------------------------------------------------
 
-echo Unpack finished! Files should be in the 'OGG' folder
+echo Unpack finished, files should be in the 'OGG' folder
 
 echo -------------------------------------------------------------
 echo.
 
 :choice
 
-set /P c=Should I delete BNKs and PCKs from the 'Game Files' folder [Y/N]?
+set /P c=Should I delete game archive files from the 'Game Files' folder [Y/N]?
 if /I "%c%" EQU "Y" goto :yes
 if /I "%c%" EQU "N" goto :hella_no
 goto :choice
 
 :yes
 
-FOR %%e IN ("Game Files\*.PCK") DO (DEL "%%e")
+FOR %%e IN ("Game Files\*.pck") DO (DEL "%%e")
+FOR %%e IN ("Game Files\*.wsp") DO (DEL "%%e")
 FOR %%f IN ("Game Files\*.BNK") DO (DEL "%%f")
 
-echo Files deleted, enjoy your unpacked audio! -/u/Vextil ;)
+echo Game archives deleted, enjoy your unpacked audio!
 pause 
 exit
 
 :hella_no
 
-echo BNKs and PCKs kept, enjoy your unpacked audio! -/u/Vextil ;)
+echo Game archives kept, enjoy your unpacked audio!
 
 pause 
 exit
